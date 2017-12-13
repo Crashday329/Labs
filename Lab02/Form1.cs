ef4c75c45516f3005351a9f47a7c18d04192e55f
crashday329 <crashday329@gmail.com>
2017-12-13 23:44:48 +0300

    рефакторинг "индусского кода": заменил алгоритм посчета частот на цикл с linq запросом старый код для сравнения оставлен в виде комментариев

--- Lab02/Form1.cs
+++ Lab02/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
@@ -33,26 +34,33 @@ namespace Lab01
             _ll.Clear();
 
             _num.Add(_a);
-            for (int i = 0; i < 10; i++)
-                _ll.Add(0);
+            //for (int i = 0; i < 10; i++)
+            //    _ll.Add(0);
             //получение сетки для графика частот
             for (int i = 1; i < 10; i++)
                 _num.Add(_num[i - 1] + (_b - _a) / 10.0);
-            
-            foreach (double d in li)
+            double delta = (_b - _a) / 10;
+            for (int i = 0; i < 10; i++)
             {
-                double delta = (_b - _a) / 10;
-                if (d < _a + delta) _ll[0]++;
-                if (d >= _a + delta && d < _a + delta * 2) _ll[1]++;
-                if (d >= _a + delta * 2 && d < _a + delta * 3) _ll[2]++;
-                if (d >= _a + delta * 3 && d < _a + delta * 4) _ll[3]++;
-                if (d >= _a + delta * 4 && d < _a + delta * 5) _ll[4]++;
-                if (d >= _a + delta * 5 && d < _a + delta * 6) _ll[5]++;
-                if (d >= _a + delta * 6 && d < _a + delta * 7) _ll[6]++;
-                if (d >= _a + delta * 7 && d < _a + delta * 8) _ll[7]++;
-                if (d >= _a + delta * 8 && d < _a + delta * 9) _ll[8]++;
-                if (d >= _a + delta * 9 && d <= _b) _ll[9]++;
+               _ll.Add( li.Select(d => d >= _a + delta * i && d < _a + delta * (i + 1)).Count());
             }
+
+
+
+            //foreach (double d in li)
+            //{
+            //    double delta = (_b - _a) / 10;
+            //    if (d < _a + delta) _ll[0]++;
+            //    if (d >= _a + delta && d < _a + delta * 2) _ll[1]++;
+            //    if (d >= _a + delta * 2 && d < _a + delta * 3) _ll[2]++;
+            //    if (d >= _a + delta * 3 && d < _a + delta * 4) _ll[3]++;
+            //    if (d >= _a + delta * 4 && d < _a + delta * 5) _ll[4]++;
+            //    if (d >= _a + delta * 5 && d < _a + delta * 6) _ll[5]++;
+            //    if (d >= _a + delta * 6 && d < _a + delta * 7) _ll[6]++;
+            //    if (d >= _a + delta * 7 && d < _a + delta * 8) _ll[7]++;
+            //    if (d >= _a + delta * 8 && d < _a + delta * 9) _ll[8]++;
+            //    if (d >= _a + delta * 9 && d <= _b) _ll[9]++;
+            //}
 
         }
 
